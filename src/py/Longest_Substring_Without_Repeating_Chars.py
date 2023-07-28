1f330fa38e83b10203e229f311d1d14188a26116
Adit Chawdhary <khac@users.noreply.github.com>
2023-07-28 13:56:19 -0700

    Create Longest_Substring_Without_Repeating_Chars.py

---
+++ src/py/Longest_Substring_Without_Repeating_Chars.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s:str) -> int:
+        repeat_char = defaultdict(int)
+        longest_substr_len = 0
+        left_index = 0
+        for right_index in range(len(s)):
+            ordinal_index = ord(s[right_index]) - ord('a')
+            repeat_char[ordinal_index] += 1
+            while repeat_char[ordinal_index] > 1:
+                cur_left_index = ord(s[left_index]) - ord('a')
+                repeat_char[cur_left_index] -= 1
+                left_index += 1
+            longest_substr_len = max(longest_substr_len, right_index-left_index+1)
+        return longest_substr_len
