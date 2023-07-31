16ef48889c79b4157b7ba2aa137692fe55ba0f5a
Adit Chawdhary <khac@users.noreply.github.com>
2023-07-31 01:56:19 -0700

    Update Word_search_II.py
    add remove word and word count references

--- src/py/Word_search_II.py
+++ src/py/Word_search_II.py
@@ -6,6 +6,7 @@ class TrieNode:
         self.chars = {}
         self.isWord = False
         self.word = None
+        self.refs = 0
 
 class Trie:
     def __init__(self):
@@ -14,12 +15,22 @@ class Trie:
     def addWord(self, word: str) -> None:
         node = self.root
         for char in word:
+            node.refs += 1
             if char not in node.chars:
                 node.chars[char] = TrieNode()
             node = node.chars[char]
+        node.refs += 1
         node.isWord = True
         node.word = word
 
+    def removeWord(self, word:str) -> None:
+        node = self.root
+        for char in word:
+            node.refs -= 1
+            node = node.chars[char]
+        node.refs -= 1
+        node.isWord = False
+       
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         trie = Trie()
@@ -28,10 +39,11 @@ class Solution:
         
         def dfs(i,j, node, result):
             if node.isWord:
-                node.isWord = False
+                # node.isWord = False 
                 result.append(node.word)
+                trie.removeWord(node.word)
 
-            if 0<=i<m and 0<=j<n and board[i][j] in node.chars:
+            if 0<=i<m and 0<=j<n and board[i][j] in node.chars and node.refs > 0:
                 temp = board[i][j]
                 board[i][j] = '#'
                 for (x,y) in [(i-1,j),(i+1,j),(i,j-1),(i,j+1)]:
